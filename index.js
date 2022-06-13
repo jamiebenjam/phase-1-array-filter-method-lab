35fac48d5ec6519f12c3ae9f510c425f4363e89b
Jamie Benjamin <bennythedev@gmail.com>
2022-06-13 12:35:25 -0500

    commit tree b6e362632d6feacd9188496077c8bcd87c3955b8

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+//takes in array of names and a string as arguments
+//returns matching list of drivers
+//case insensitive 
+//localeCompare(compareString)
+//  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(collection, string) {
+    const newCollection = [];
+    for (const user of collection) {
+        if (user === string) {
+      return(newCollection.push(user));
+    }
+    }
+  }
+
+  
+
